cdd969f3c3f7310201bbeec5d98e59d808572892
CandyShock <mrhell699@gmail.com>
2023-04-22 17:05:00 +0300

    sd4

---
+++ homework-2/utils.py
@@ -0,0 +1,11 @@
+import csv
+
+
+def sds():
+    with open('items.csv', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            emp_str1 = (row['name'], row['price'], row['quantity'])
+            return ", ".join(emp_str1)
+
+sds()
