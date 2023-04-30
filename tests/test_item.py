85992e6113016673247162e439e52dc0b2e1e5d4
CandyShock <mrhell699@gmail.com>
2023-04-30 12:56:28 +0300

    dz 4

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,16 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
+from src.phone import Phone
+
 
 @pytest.fixture
 def item_obj():
     return Item("name", 2, 12)
+
+
 @pytest.fixture
 def item_emp():
     return Item('name', 2, 14)
 
+
 def test_func(item_obj):
     assert item_obj.name == "name"
+
 
 def test_apply_discount(item_obj):
     assert item_obj.apply_discount() == 2
@@ -20,17 +26,19 @@ def test_calculate_total(item_obj):
     assert item_obj.calculate_total_price() == 24
 
 
-
 def test_string_to_number():
     assert Item.string_to_number('5') == 5
     assert Item.string_to_number('5.0') == 5
     assert Item.string_to_number('5.7') == 5
     assert Item.string_to_number('6.0') == 6
 
+
 def test_instantiate_from_csv():
-    emp = [['Смартфон', '100', '1'], ['Ноутбук', '1000', '3'], ['Кабель', '10', '5'], ['Мышка', '50', '5'], ['Клавиатура', '75', '5']]
+    emp = [['Смартфон', '100', '1'], ['Ноутбук', '1000', '3'], ['Кабель', '10', '5'], ['Мышка', '50', '5'],
+           ['Клавиатура', '75', '5']]
     Item.instantiate_from_csv(emp)
     assert len(Item.all) == 5
+
 
 def test_repr():
     item1 = Item("Смартфон", 10000, 20)
@@ -38,13 +46,18 @@ def test_repr():
     assert repr(item1) == "Item('Смартфон', 10000, 20)"
     assert repr(item2) == "Item('Калькулятор', 20, 100)"
 
+
 def test_str():
     item1 = "Смартфон"
     item2 = "Калькулятор"
-    assert  str(item1) == 'Смартфон'
-    assert  str(item2) == 'Калькулятор'
+    assert str(item1) == 'Смартфон'
+    assert str(item2) == 'Калькулятор'
+
 
 def test_check():
     test = Item("Смартфон", 10000, 20)
     test1 = Item("Смартфон", 10000, 20)
+    test2 = Phone("Смартфон", 10000, 25, 2)
     assert test + test1 == 40
+    assert test2 + test2 == 50
+    assert test + test2 == 45
